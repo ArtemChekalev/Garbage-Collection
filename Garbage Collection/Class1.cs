54b09c18fbc7f5558a5938dc74af6a362f0c69db
Артём Чекалев <st087200@student.spbu.ru>
2021-06-03 20:01:20 +0300

    Добавьте файлы проекта.

---
+++ Garbage Collection/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garbage_Collection
+{
+
+    class Class1
+    {
+        public List<long> objects = new List<long>();
+        private int number;
+        private bool flag;
+        public Class1(int Num, bool Flag)
+        {
+            this.number = Num;
+            this.flag = Flag;
+            for (int i = 0; i < 10000; i++) objects.Add(i);
+        }
+        ~Class1() { if (flag) Console.Write(number + " "); }
+    }
+}
---
+++ Garbage Collection/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Garbage_Collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                Console.Write($"Создан элемент {i}, удалён: ");
+                Class1 obj = new Class1(i, true);
+                GC.WaitForPendingFinalizers();
+                Console.WriteLine();
+            }
+            
+            CheckTime(100000);
+        }
+        private static void CheckTime(int n)
+        {
+            Class1[] classes = new Class1[n];
+            for (int i = 0; i < classes.Length; i++) classes[i] = (new Class1(i, false));
+            classes = null;
+            DateTime begin = DateTime.Now;
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            DateTime end = DateTime.Now;
+            var delta = end - begin; 
+            Console.WriteLine();
+            Console.WriteLine($"Сборка мусора работала {delta.TotalMilliseconds + " милисекунд"} с {n} объектами"  );
+        }
+    }
+}
